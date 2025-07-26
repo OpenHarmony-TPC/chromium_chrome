6a1bd74a3e6c6b80a31c927013a8ce40ec5b4199
a9443511 <liangyi34@h-partners.com>
2025-07-26 17:55:44 +0800

    revert declarativeNetRequest action count 适配
    Signed-off-by: a9443511 <liangyi34@h-partners.com>

--- browser/extensions/extension_action_runner.cc
+++ browser/extensions/extension_action_runner.cc
@@ -51,10 +51,6 @@
 #include "extensions/common/permissions/permissions_data.h"
 #include "url/origin.h"
 
-#if BUILDFLAG(ARKWEB_ARKWEB_EXTENSIONS)
-#include "chrome/browser/extensions/extension_tab_util.h"
-#endif
-
 namespace {
 
 std::vector<extensions::ExtensionId> GetExtensionIds(
@@ -458,11 +454,6 @@ void ExtensionActionRunner::RunBlockedActions(const Extension* extension) {
 
 void ExtensionActionRunner::DidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
-#if BUILDFLAG(ARKWEB_ARKWEB_EXTENSIONS)
-  if (!web_contents() || ExtensionTabUtil::GetTabId(web_contents()) < 0) {
-    return;
-  }
-#endif
   declarative_net_request::RulesMonitorService* rules_monitor_service =
       declarative_net_request::RulesMonitorService::Get(browser_context_);
 
@@ -506,11 +497,6 @@ void ExtensionActionRunner::DidFinishNavigation(
 }
 
 void ExtensionActionRunner::WebContentsDestroyed() {
-#if BUILDFLAG(ARKWEB_ARKWEB_EXTENSIONS)
-  if (!web_contents() || ExtensionTabUtil::GetTabId(web_contents()) < 0) {
-    return;
-  }
-#endif
   ExtensionActionDispatcher::Get(browser_context_)
       ->ClearAllValuesForTab(web_contents());
 
